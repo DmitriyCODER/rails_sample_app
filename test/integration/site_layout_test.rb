848442466730881ede19dc1d5ee940a53f222a0b
Dmitriy Veshnyakov <dmitriyvs85@mail.ru>
2021-09-20 09:55:29 +0700

    Make a basic User model (including secure passwords)

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,11 +6,11 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   # end
 
   test "layout links" do
-    #get '/'
-    #assert_template 'static_pages/home'
-    #assert_select "a[href=?]", root_path, count: 2
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", help_path
-    #assert_select "a[href=?]", about_path
-    #assert_select "a[href=?]", contact_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
   end
 end
